754249c043f08f1bbee44e2b332ee1c9c7e16c2e
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-24 20:13:21 +0900

    [update] Start the tutorial from the beginning.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
 var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false
-    }
-  },
+  type: Phaser.AUTO, // レンダラーを指定する
+  width: 800, // ゲームの幅
+  height: 600, // ゲームの高さ
+
+  // ゲームに追加するシーン
   scene: {
     preload: preload,
     create: create,
@@ -16,77 +11,27 @@ var config = {
   }
 }
 
-var game = new Phaser.Game(config)
+var game = new Phaser.Game(config) // ゲーム全体のコントローラー
 
+// ゲーム開始前に呼び出される関数定義
 function preload() {
-  this.load.image('sky', 'assets/sky.png')
+  this.load.image('sky', 'assets/sky.png') // イメージをロードする
   this.load.image('ground', 'assets/platform.png')
   this.load.image('star', 'assets/star.png')
   this.load.image('bomb', 'assets/bomb.png')
+
+  // スプライトシートをロードする
   this.load.spritesheet('dude', 'assets/dude.png', {
     frameWidth: 32,
     frameHeight: 48
   })
 }
 
+// ゲーム開始時に呼び出される関数
 function create() {
-  this.add.image(400, 300, 'sky')
-
-  platforms = this.physics.add.staticGroup()
-
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody()
-
-  platforms.create(600, 400, 'ground')
-  platforms.create(50, 250, 'ground')
-  platforms.create(750, 220, 'ground')
-
-  player = this.physics.add.sprite(100, 450, 'dude')
-
-  player.setBounce(0.2)
-  player.setCollideWorldBounds(true)
-
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-  })
-
-  this.anims.create({
-    key: 'turn',
-    frames: [{ key: 'dude', frame: 4 }],
-    frameRate: 20
-  })
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  })
-
-  cursors = this.input.keyboard.createCursorKeys() // キーボードのカーソルを使用する
-
-  this.physics.add.collider(player, platforms)
+  this.add.image(400, 300, 'sky') // 新しいイメージオブジェクトを作成し、シーンに追加する
+  this.add.image(400, 300, 'star')
 }
 
-function update() {
-  // ポーリング(非同期に発生するイベントをチェック)する
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160)
-
-    player.anims.play('left', true)
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160)
-
-    player.anims.play('right', true)
-  } else {
-    player.setVelocityX(0)
-
-    player.anims.play('turn')
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330)
-  }
-}
+// ゲーム進行時に呼び出される関数
+function update() {}
